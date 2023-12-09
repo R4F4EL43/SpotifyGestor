0bcca24842d2364df999ef5c6e4f88d585339875
Gustavo Alcantara <I210091@inete.net>
2023-12-09 16:49:33 +0000

    é isto

---
+++ SpotifyGestor/Frm_Admin.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpotifyGestor
+{
+    public partial class Frm_Admin : Form
+    {
+        #region Metodos proprios
+
+        public void FillListView()
+        {
+            lvw_Contas.Items.Clear();
+            foreach (Conta conta in Variaveis.Contas)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Tag = conta.IdConta;
+                item.Text = conta.Nome;
+                item.SubItems.Add(conta.Email);
+                item.SubItems.Add(conta.Password);
+                item.SubItems.Add(conta.IsAdmin.ToString());
+
+                lvw_Contas.Items.Add(item);
+            }
+        }
+        #endregion
+
+        public Frm_Admin()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos
+
+        #region Clicks
+
+        private void lbl_ListaContas_Click(object sender, EventArgs e)
+        {
+            pnl_ListaContas.Visible = true;
+            pnl_CriarPlaylist.Visible = false;
+            pnl_logOut.Visible = false;
+
+            //Preencher ListView...            
+            FillListView();
+
+        }
+
+        private void lbl_CriarPlaylist_Click(object sender, EventArgs e)
+        {
+            pnl_CriarPlaylist.Visible = true;
+            pnl_ListaContas.Visible = false;
+            pnl_logOut.Visible = false;
+        }
+
+        private void lbl_LogOutGeral_Click(object sender, EventArgs e)
+        {
+            pnl_logOut.Visible = true;
+            pnl_CriarPlaylist.Visible = false;
+            pnl_ListaContas.Visible = false;
+        }
+
+        private void btn_TerminarSessão_Click(object sender, EventArgs e)
+        {
+            //Terminar sessão
+        }
+
+        private void btn_CriarPlaylist_Click(object sender, EventArgs e)
+        {
+            //Criar playlist
+        }
+
+        private void btn_CriarConta_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form2 = new Frm_CriarConta();
+            form2.Closed += (s, args) => this.Close();
+            form2.Show();
+        }
+
+        private void btn_EditarConta_Click(object sender, EventArgs e)
+        {
+            if (lvw_Contas.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Selecione um item para poder editar.", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                //pegar a conta a passar para o form Editar
+                Conta conta = Variaveis.Contas.FirstOrDefault(s => s.Nome == lvw_Contas.SelectedItems[0].Text);
+
+                this.Hide();
+                var form2 = new Frm_EditarConta(conta);
+                form2.Closed += (s, args) => this.Close();
+                form2.Show();
+            }
+        }
+
+        private void btn_EliminarConta_Click(object sender, EventArgs e)
+        {
+            if (lvw_Contas.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Selecione um item para poder eliminar.", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {                
+                if (MessageBox.Show($"Tem a certeza de que quer eliminar {lvw_Contas.SelectedItems[0].Text}?", string.Empty, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    //Eliminar da List<Contas>
+                    Conta conta = Variaveis.Contas.FirstOrDefault(s => s.Nome == lvw_Contas.SelectedItems[0].Text);
+                    Artista artista = Variaveis.Artistas.FirstOrDefault(s => s.Conta == conta);
+
+                    Variaveis.Contas.Remove(conta);
+
+                    if (artista != null)
+                        Variaveis.Artistas.Remove(artista);
+
+                    FillListView();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Label:Hover
+
+        private void lbl_ListaContas_MouseHover(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Hand;
+            lbl_ListaContas.Font = new Font(lbl_ListaContas.Font, FontStyle.Bold);
+            lbl_ListaContas.Font = new Font(lbl_ListaContas.Font, FontStyle.Underline);
+        }
+
+        private void lbl_CriarPlaylist_MouseHover(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Hand;
+            lbl_CriarPlaylist.Font = new Font(lbl_CriarPlaylist.Font, FontStyle.Bold);
+            lbl_CriarPlaylist.Font = new Font(lbl_CriarPlaylist.Font, FontStyle.Underline);
+        }
+
+        private void lbl_LogOutGeral_MouseHover(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Hand;
+            lbl_LogOutGeral.Font = new Font(lbl_LogOutGeral.Font, FontStyle.Bold);
+            lbl_LogOutGeral.Font = new Font(lbl_LogOutGeral.Font, FontStyle.Underline);
+        }
+
+        #endregion
+
+        #region Label:Leave
+
+        private void lbl_ListaContas_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+            lbl_ListaContas.Font = new Font(lbl_ListaContas.Font, FontStyle.Regular);
+        }
+
+        private void lbl_CriarPlaylist_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+            lbl_CriarPlaylist.Font = new Font(lbl_CriarPlaylist.Font, FontStyle.Regular);
+        }
+
+        private void lbl_LogOutGeral_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+            lbl_LogOutGeral.Font = new Font(lbl_LogOutGeral.Font, FontStyle.Regular);
+        }
+
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ SpotifyGestor/Frm_CriarConta.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SpotifyGestor
+{
+    partial class Frm_CriarConta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_CriarConta = new System.Windows.Forms.Button();
+            this.chb_IsAdmin = new System.Windows.Forms.CheckBox();
+            this.txt_Senha = new System.Windows.Forms.TextBox();
+            this.lbl_Senha = new System.Windows.Forms.Label();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.lbl_Email = new System.Windows.Forms.Label();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.lbl_Nome = new System.Windows.Forms.Label();
+            this.lbl_CriarConta = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_CriarConta
+            // 
+            this.btn_CriarConta.BackColor = System.Drawing.SystemColors.MenuText;
+            this.btn_CriarConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CriarConta.ForeColor = System.Drawing.Color.Red;
+            this.btn_CriarConta.Location = new System.Drawing.Point(348, 412);
+            this.btn_CriarConta.Name = "btn_CriarConta";
+            this.btn_CriarConta.Size = new System.Drawing.Size(74, 37);
+            this.btn_CriarConta.TabIndex = 33;
+            this.btn_CriarConta.Text = "Criar";
+            this.btn_CriarConta.UseVisualStyleBackColor = false;
+            this.btn_CriarConta.Click += new System.EventHandler(this.btn_CriarConta_Click);
+            // 
+            // chb_IsAdmin
+            // 
+            this.chb_IsAdmin.AutoSize = true;
+            this.chb_IsAdmin.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chb_IsAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chb_IsAdmin.Location = new System.Drawing.Point(17, 273);
+            this.chb_IsAdmin.Name = "chb_IsAdmin";
+            this.chb_IsAdmin.Size = new System.Drawing.Size(78, 24);
+            this.chb_IsAdmin.TabIndex = 32;
+            this.chb_IsAdmin.Text = "Admin";
+            this.chb_IsAdmin.UseVisualStyleBackColor = true;
+            // 
+            // txt_Senha
+            // 
+            this.txt_Senha.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txt_Senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Senha.ForeColor = System.Drawing.SystemColors.Control;
+            this.txt_Senha.Location = new System.Drawing.Point(17, 229);
+            this.txt_Senha.Name = "txt_Senha";
+            this.txt_Senha.Size = new System.Drawing.Size(405, 26);
+            this.txt_Senha.TabIndex = 31;
+            // 
+            // lbl_Senha
+            // 
+            this.lbl_Senha.AutoSize = true;
+            this.lbl_Senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Senha.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_Senha.Location = new System.Drawing.Point(13, 206);
+            this.lbl_Senha.Name = "lbl_Senha";
+            this.lbl_Senha.Size = new System.Drawing.Size(61, 20);
+            this.lbl_Senha.TabIndex = 30;
+            this.lbl_Senha.Text = "Senha";
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txt_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Email.ForeColor = System.Drawing.SystemColors.Control;
+            this.txt_Email.Location = new System.Drawing.Point(17, 172);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(405, 26);
+            this.txt_Email.TabIndex = 29;
+            // 
+            // lbl_Email
+            // 
+            this.lbl_Email.AutoSize = true;
+            this.lbl_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Email.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_Email.Location = new System.Drawing.Point(13, 149);
+            this.lbl_Email.Name = "lbl_Email";
+            this.lbl_Email.Size = new System.Drawing.Size(53, 20);
+            this.lbl_Email.TabIndex = 28;
+            this.lbl_Email.Text = "Email";
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txt_Nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nome.ForeColor = System.Drawing.SystemColors.Control;
+            this.txt_Nome.Location = new System.Drawing.Point(17, 115);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(405, 26);
+            this.txt_Nome.TabIndex = 27;
+            // 
+            // lbl_Nome
+            // 
+            this.lbl_Nome.AutoSize = true;
+            this.lbl_Nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nome.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_Nome.Location = new System.Drawing.Point(13, 92);
+            this.lbl_Nome.Name = "lbl_Nome";
+            this.lbl_Nome.Size = new System.Drawing.Size(55, 20);
+            this.lbl_Nome.TabIndex = 26;
+            this.lbl_Nome.Text = "Nome";
+            // 
+            // lbl_CriarConta
+            // 
+            this.lbl_CriarConta.AutoSize = true;
+            this.lbl_CriarConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_CriarConta.ForeColor = System.Drawing.Color.Red;
+            this.lbl_CriarConta.Location = new System.Drawing.Point(12, 9);
+            this.lbl_CriarConta.Name = "lbl_CriarConta";
+            this.lbl_CriarConta.Size = new System.Drawing.Size(145, 29);
+            this.lbl_CriarConta.TabIndex = 25;
+            this.lbl_CriarConta.Text = "Criar Conta";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SpotifyGestor.Properties.Resources.arrow;
+            this.pictureBox1.Location = new System.Drawing.Point(392, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 34;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Frm_CriarConta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_CriarConta);
+            this.Controls.Add(this.chb_IsAdmin);
+            this.Controls.Add(this.txt_Senha);
+            this.Controls.Add(this.lbl_Senha);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.lbl_Email);
+            this.Controls.Add(this.txt_Nome);
+            this.Controls.Add(this.lbl_Nome);
+            this.Controls.Add(this.lbl_CriarConta);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Frm_CriarConta";
+            this.Text = "Criar Conta";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_CriarConta;
+        private System.Windows.Forms.CheckBox chb_IsAdmin;
+        private System.Windows.Forms.TextBox txt_Senha;
+        private System.Windows.Forms.Label lbl_Senha;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label lbl_Email;
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.Label lbl_Nome;
+        private System.Windows.Forms.Label lbl_CriarConta;
+    }
+}
